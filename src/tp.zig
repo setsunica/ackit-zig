a98311249b80e895ff02579f94d69bfcf7c2ac56
setsunica <31218125+setsunica@users.noreply.github.com>
2023-12-13 02:46:26 +0900

    Update tp.zig

--- src/tp.zig
+++ src/tp.zig
@@ -4,49 +4,90 @@ const Allocator = std.mem.Allocator;
 const StructField = builtin.Type.StructField;
 const ArrayList = std.ArrayList;
 
-const Input = std.json.ArrayHashMap([]u8);
-// const Input = struct { a: []u8 };
+pub fn Parsed(comptime T: type) type {
+    return struct {
+        alloc: Allocator,
+        value: T,
 
-pub fn Parse(comptime T: type, comptime content: []const u8) !type {
-    _ = T;
-    var allocator = std.heap.page_allocator;
-    var parsed = try std.json.parseFromSlice(Input, allocator, content, .{});
-    defer parsed.deinit();
-    var iter = parsed.value.map.iterator();
-    var writer = std.io.getStdOut().writer();
-    _ = writer;
-    // const fields = ArrayList(StructField).init(allocator);
-    // fields.deinit();
-    const fields: [1]StructField = undefined;
-    var i: u8 = 0;
-    while (iter.next()) |f| : (i += 1) {
-        const t = if (std.mem.eql(u8, f.value_ptr.*, "i32")) {
-            return i32;
-        } else {
-            unreachable;
-        };
-        fields[i] = .{ .name = f.key_ptr.*, .field_type = t, .default_value = null, .is_comptime = false, .alignment = 0 };
-        // fields.append(.{ .name = f.key_ptr.*, .field_type = t, .default_value = null, .is_comptime = false, .alignment = 0 });
-        // try writer.print("key: {s}, value: {s}", .{ f.key_ptr.*, f.value_ptr.* });
-    }
-    return @Type(.{
-        .Struct = .{
-            .layout = .Auto,
-            .fields = fields,
-            //     &[_]builtin.TypeInfo.StructField{
-            //     .{ .name = "one", .field_type = i32, .default_value = null, .is_comptime = false, .alignment = 0 },
-            // },
-            .decls = &[_]builtin.TypeInfo.Declaration{},
-            .is_tuple = false,
-        },
-    });
+        fn init(value: T, allocator: Allocator) Parsed(T) {
+            return .{ .alloc = allocator, .value = value };
+        }
+    };
 }
 
-pub fn hoge() !void {
-    const content =
-        \\{"a": "b"}
-    ;
-    var parsed = try std.json.parseFromSlice(Input, std.heap.page_allocator, content, .{});
-    defer parsed.deinit();
-    try std.io.getStdOut().writer().print("{any}\n", .{parsed.value.map.get("a").?});
+const Cursor = struct {
+    // arena: *std.heap.ArenaAllocator,
+    input: []const u8,
+    // pos: u32,
+    line_iter: std.mem.SplitIterator(u8, std.mem.DelimiterType.scalar),
+    word_iter: std.mem.SplitIterator(u8, std.mem.DelimiterType.scalar),
+
+    fn init(arena: *std.heap.ArenaAllocator, input: []const u8) Cursor {
+        _ = arena;
+        return Cursor{
+            // .arena = arena,
+            .input = input,
+            // .pos = 0,
+            .line_iter = std.mem.splitScalar(u8, input, '\n'),
+            .word_iter = std.mem.splitScalar(u8, input, ' '),
+        };
+    }
+
+    fn next(self: *Cursor) ?[]const u8 {
+        if (self.word_iter.next()) |w| {
+            return w;
+        } else if (self.line_iter.next()) |l| {
+            self.word_iter = std.mem.splitScalar(u8, l, ' ');
+            return self.next();
+        }
+        return null;
+    }
+
+    fn nextLine(self: *Cursor) ?[]const u8 {
+        if (self.line_iter.next()) |l| {
+            self.word_iter = std.mem.splitScalar(u8, l, ' ');
+            return l;
+        }
+        return null;
+    }
+};
+
+fn parseField(comptime T: type, field: StructField, cur: *Cursor) !T {
+    switch (@typeInfo(field.type)) {
+        .Bool => {
+            var next = cur.next();
+            return if (std.mem.eql(u8, next, "true")) {
+                true;
+            } else if (std.mem.eql(u8, next, "false")) {
+                false;
+            } else return {
+                error{ParseBoolError};
+            };
+        },
+        .Int => |i| try std.fmt.parseInt(i.type, cur.next(), 10),
+        .Float => |f| try std.fmt.parseFloat(f.type, cur.next()),
+        // .Array => |arr| unreachable,
+        else => @compileError(""),
+    }
+}
+
+pub fn parse(comptime T: type, allocator: Allocator, input: []const u8) !Parsed(T) {
+    // std.json.parseFree();
+    var arena = try allocator.create(std.heap.ArenaAllocator);
+    var arena_allocator = arena.allocator();
+    var cur = &Cursor.init(arena, input);
+    const v = switch (@typeInfo(T)) {
+        inline .Struct => |s| {
+            const v: T = undefined;
+            inline for (0..s.fields.len) |i| {
+                const field_type = comptime {
+                    return s.fields[i].type;
+                };
+                @field(v, s.fields[i].name) = try parseField(field_type, s.fields[i], cur);
+            }
+            return v;
+        },
+        inline else => @compileError("Failed to parse input. No support for non-structures."),
+    };
+    return Parsed(T).init(v, arena_allocator);
 }
