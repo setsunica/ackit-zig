3c6aa13396c3223e741ea2739e3f903249ee19be
setsunica <muxwld@gmail.com>
2023-12-14 01:55:15 +0900

    Update tp.zig

--- src/tp.zig
+++ src/tp.zig
@@ -90,7 +90,7 @@ const Scanner = struct {
         return try Scanner.parse(T, w);
     }
 
-    fn parseNextLine(self: Scanner, comptime T: type) ![]T {
+    fn parseNextLine(self: *Scanner, comptime T: type) ![]T {
         var arr = ArrayList(T).init(self.arena.allocator());
         var li = self.cur.nextLine() orelse return error.NoNextLine;
         while (li.next()) |w| {
@@ -100,7 +100,7 @@ const Scanner = struct {
         return try arr.toOwnedSlice();
     }
 
-    fn parseNextLineArray(self: Scanner, comptime T: type, comptime len: usize) !?[len]T {
+    fn parseNextLineArray(self: *Scanner, comptime T: type, comptime len: usize) !?[len]T {
         var arr: [len]T = undefined;
         const li = if (self.cur.nextLine()) |l| l else return null;
         for (li, 0..) |w, i| {
@@ -109,19 +109,19 @@ const Scanner = struct {
         }
         return arr;
     }
-};
 
-fn parseField(comptime T: type, scanner: *Scanner) !T {
-    return switch (@typeInfo(T)) {
-        .Bool, .Int, .Float => try scanner.parseNext(T),
-        .Pointer => |p| if (p.size == .Slice)
-            try scanner.parseNextLine(p.child)
-        else
-            @compileError("invalid field, non-slice pointers are not supported"),
-        .Array => |arr| try scanner.parseNextLineArray(arr.child, arr.len),
-        else => @compileError("invalid field, unsupported field types"),
-    };
-}
+    fn parseField(self: *Scanner, comptime T: type) !T {
+        return switch (@typeInfo(T)) {
+            .Bool, .Int, .Float => try self.parseNext(T),
+            .Pointer => |p| if (p.size == .Slice)
+                try self.parseNextLine(p.child)
+            else
+                @compileError("invalid field, non-slice pointers are not supported"),
+            .Array => |arr| try self.parseNextLineArray(arr.child, arr.len),
+            else => @compileError("invalid field, unsupported field types"),
+        };
+    }
+};
 
 pub fn parse(comptime T: type, allocator: Allocator, input: []const u8) !Parsed(T) {
     // std.json.parseFree();
@@ -132,7 +132,7 @@ pub fn parse(comptime T: type, allocator: Allocator, input: []const u8) !Parsed(
             var v: T = undefined;
             inline for (0..s.fields.len) |i| {
                 const field = comptime s.fields[i];
-                @field(v, field.name) = try parseField(field.type, &scanner);
+                @field(v, field.name) = try scanner.parseField(field.type);
             }
             return Parsed(T).init(v, arena);
         },
