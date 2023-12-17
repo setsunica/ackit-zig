e6ce23ef534e5f33f6a6022d8c57649542a75c93
setsunica <31218125+setsunica@users.noreply.github.com>
2023-12-17 23:43:00 +0900

    Implement an input parser

--- src/main.zig
+++ src/main.zig
@@ -1,33 +1,19 @@
 const std = @import("std");
 const tp = @import("tp.zig");
 
-const MyType = struct { a: []const u8 };
+const Input = struct { a: i32, b: f64 };
 
 pub fn main() !void {
-    const input =
-        \\{"a": "b"}
-    ;
     var allocator = std.heap.page_allocator;
-    _ = try tp.parse(MyType, allocator, input);
     const stdin_file = std.io.getStdIn().reader();
     const stdout_file = std.io.getStdOut().writer();
     var br = std.io.bufferedReader(stdin_file);
     var bw = std.io.bufferedWriter(stdout_file);
     const stdin = br.reader();
     const stdout = bw.writer();
-    const line_max_bytes = 128;
-
-    var line_buf: [line_max_bytes]u8 = undefined;
-    const line = try stdin.readUntilDelimiter(&line_buf, '\n');
-    const trimed_line = std.mem.trimRight(u8, line, &[_]u8{13});
-    var iter = std.mem.split(u8, trimed_line, " ");
-    var i: u32 = 0;
-    while (iter.next()) |c| : (i += 1) {
-        // if (c.len <= 0) {
-        //     break;
-        // }
-        try stdout.print("i = {}, c = {any}\n", .{ i, c });
-    }
-
+    const s = try stdin.readAllAlloc(allocator, 1024 * 4);
+    var input = try tp.parse(Input, allocator, s);
+    defer input.deinit();
+    try stdout.print("{any}", .{input.value});
     try bw.flush();
 }
