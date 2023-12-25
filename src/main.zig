923e561982029d7f709a2fc331cfcb85ad3a6ce8
setsunica <muxwld@gmail.com>
2023-12-26 01:00:48 +0900

    Adjust the names of structures related to standard input/output

--- src/main.zig
+++ src/main.zig
@@ -4,7 +4,7 @@ const io = @import("io.zig");
 const Input = struct { s: []const u8 };
 const Output = struct { s: []const u8 };
 
-fn solve(input: Input, printer: *io.StdOutPrinter) !void {
+fn solve(input: Input, printer: *io.Printer(io.StdOutWriter)) !void {
     const output = .{ .s = input.s };
     try printer.print(output);
 }
@@ -13,5 +13,5 @@ pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer if (gpa.deinit()) @panic("gpa memmory leaked");
     const input_max_size = 1024 * 1024 * 512;
-    try io.interactStdIO(Input, gpa.allocator(), input_max_size, solve);
+    try io.interactStdio(Input, gpa.allocator(), input_max_size, solve);
 }
