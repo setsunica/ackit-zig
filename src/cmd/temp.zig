914c2a9579c9c2776cfd20d45365cda695fe7202
setsunica <muxwld@gmail.com>
2023-12-29 03:16:43 +0900

    Add log scope

--- src/cmd/temp.zig
+++ src/cmd/temp.zig
@@ -1,5 +1,5 @@
 const std = @import("std");
-const log = std.log;
+const log = std.log.scoped(.ackit);
 const Allocator = std.mem.Allocator;
 const ArenaAllocator = std.heap.ArenaAllocator;
 const ArgIterator = std.process.ArgIterator;
