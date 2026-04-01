f2e22d8586b669ab52713b74785f217dd2a3fc05
setsunica <31218125+setsunica@users.noreply.github.com>
2026-04-02 01:35:25 +0900

    Using `StackFallbackAllocator` on an experimental basis

--- src/main.zig
+++ src/main.zig
@@ -1,13 +1,24 @@
 const std = @import("std");
+const builtin = @import("builtin");
+const heap = std.heap;
 const log = std.log.scoped(.ackit);
 const Allocator = std.mem.Allocator;
 const ArgIterator = std.process.ArgIterator;
 const cmd = @import("cmd.zig");
 
 pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer _ = gpa.deinit();
-    const allocator = gpa.allocator();
+    var da: heap.DebugAllocator(.{}) = .init;
+    var sfa = heap.stackFallback(4096, heap.page_allocator);
+
+    const allocator, const is_debug = switch (builtin.mode) {
+        .Debug, .ReleaseSafe => .{ da.allocator(), true },
+        .ReleaseFast, .ReleaseSmall => .{ sfa.get(), false },
+    };
+
+    defer {
+        if (is_debug) _ = da.deinit();
+    }
+
     var args = try ArgIterator.initWithAllocator(allocator);
     defer args.deinit();
 
